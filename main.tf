810089f29c5b7ec9fc88308460cfe9eca030a883
VENKATESH NANDIPATI <117563148+venkynandipati@users.noreply.github.com>
2023-02-15 21:10:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA4QUKASHDSEKLAZKU"
-  secret_key = "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"
+  access_key = "AKIAVIVTVCXM2IMXBOUO"
+  secret_key = "uqvMEzXEIoUfMQka4RE8/XIrl+7gmR0y8SOBh6z7"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0e6329e222e662a52"
+  ami           = "ami-04de5880b95cc889b"
   instance_type = var.instance_type
   tags= {
     Name = "raham"
